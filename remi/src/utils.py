b82150119490dac1b43fe66a3657a69e458397ac
uc-steffenr <steffenr@mail.uc.edu>
2025-04-05 16:45:27 -0400

    updated requirements and started examples

--- remi/src/utils.py
+++ remi/src/utils.py
@@ -1,15 +1,17 @@
 """Utilities for simulation."""
+import os
 import dill
 
+_pth = os.path.join('/', *__file__.split('/')[:-1])
 
 def load_position_func():
-    with open('functions/position.pkl', 'rb') as f:
+    with open(os.path.join(_pth, 'functions', 'position.pkl'), 'rb') as f:
         return dill.load(f)
 
 def load_velocity_func():
-    with open('functions/velocity.pkl', 'rb') as f:
+    with open(os.path.join(_pth, 'functions', 'velocity.pkl'), 'rb') as f:
         return dill.load(f)
 
 def load_dynamics_func():
-    with open('functions/dynamics.pkl', 'rb') as f:
+    with open(os.path.join(_pth, 'functions', 'dynamics.pkl'), 'rb') as f:
         return dill.load(f)
